94910380cb5cb4f84f04fcf68bda6c343acc44a9
Webb Hinton <wyhinton@ncsu.edu>
2021-10-03 17:58:24 -0400

    impl svg based playhead

--- src/components/SampleTray/Sample/Waveform.tsx
+++ src/components/SampleTray/Sample/Waveform.tsx
@@ -27,10 +27,15 @@ const containerClass = classNames("waveform-container", {
   "popup-3d": hovered,
   // "popdown-3d": !hovered,
 });
+const playHeadWidth = 10; 
 const boxWidth = mapRange(length, 0, 3, 0, 400);
-const playheadPosition = mapRange(sampleProgress, 0, 1, 0, boxWidth)
+const playheadPosition = mapRange(sampleProgress, 0, 1, 0, boxWidth+playHeadWidth )
 const strokeWidth = 0;
+const maskId = `playHeadMask_${sampleData.length}`
+const pathRef = `url(#${maskId})`
 
+console.log(maskId);
+console.log(pathRef);
   return(
     <div className ={containerClass} onMouseEnter = {(e)=>{setHovered(true)}} onMouseLeave = {(e)=>{setHovered(false)}}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox={createViewBox(length)} style = {{width: "fit-content", height: 90}}>
@@ -41,23 +46,16 @@ const strokeWidth = 0;
               <stop offset='47.525%' stop-color='#53A0FD'  /> 
               {/* <stop offset='100%' stop-color='#B4EC51'  />  */}
         </linearGradient>
-        <clipPath id="myClip">
-          <g transform={"translate(0 50)"}>  
+        <clipPath id={maskId}>
            <path d={svgPath}/>
-          </g>
         </clipPath>
-        <clipPath id="rectClip">
-            {/* <rect x = {playheadPosition} y ={-100} id = "playhead" width ={100} fill = {"green"} height={400}></rect>  */}
-            <rect x = {playheadPosition} y ={-100} id = "playhead" width ={100} fill = {"green"} height={400}></rect> 
-        </clipPath>
+        <filter id="blur">
+        <feGaussianBlur stdDeviation="2" />
+        </filter>
       </defs>
-        {/* <g>   */}
         <g transform={"translate(0 50)"}>  
-          {/* <rect width ={100} fill = {"green"} height={100}></rect>  */}
           <path d={svgPath} stroke = {"red"} fill = {"url(#bgGradient)"} strokeWidth ={strokeWidth}/>
-          {/* <path d={svgPath} stroke = {"red"} fill = {"red"} strokeWidth ={strokeWidth} /> */}
-          {/* <path d={svgPath} stroke = {"red"} fill = {"red"} strokeWidth ={strokeWidth} clipPath="url(#rectClip)"/> */}
-          <rect x = {playheadPosition} y ={-100} id = "playhead" width ={100} fill = {"green"} height={400} clipPath="url(#myClip)"></rect> 
+          <rect filter = "url(#blur)" x = {playheadPosition-playHeadWidth} y ={-100} id = "playhead" width ={playHeadWidth} fill = {"white"} height={400} clipPath={pathRef}></rect> 
         </g>
       </svg>
 

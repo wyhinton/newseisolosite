9302aa32609d679ccd569f19c5448eede859395d
wyhinton <carolinabmx11@gmail.com>
2022-01-18 21:32:43 -0500

    ss

--- src/pages/Testing/CSS3DDemo.tsx
+++ src/pages/Testing/CSS3DDemo.tsx
@@ -3,13 +3,15 @@ import {
   OrbitControls,
   OrthographicCamera,
   PerspectiveCamera,
+  Tube,
 } from "@react-three/drei";
 import { HtmlProps } from "@react-three/drei/web/Html";
-import { useSpring } from "framer-motion";
-import React, { useEffect, useRef, useState } from "react";
+import { animate, useMotionValue, useSpring } from "framer-motion";
+import { useControls } from "leva";
+import React, { forwardRef, useEffect, useMemo, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 import { Canvas, extend, useFrame, useThree } from "react-three-fiber";
-import { Group, Matrix4, Mesh, Vector3 } from "three";
+import { CatmullRomCurve3, Color, Group, Matrix4, Mesh, Object3D, Quaternion, Vector3 } from "three";
 import { CSS3DObject } from "three/examples/jsm/renderers/CSS3DRenderer";
 // import { CameraControls } from "./CameraControls";
 // import { Physics, PlaneProps, useBox, usePlane } from "@react-three/cannon";
@@ -31,74 +33,78 @@ function Plane(): JSX.Element {
     </>
   );
 }
-const World = ({ trig }: { trig: number }): JSX.Element => {
-  // const [ref, api] = useBox(() => ({ mass: 1, angularDamping: 0.8 }))
-  // useEffect(() => {
-  //   const listener = (e: KeyboardEvent) => {
-  //     if (e.keyCode === 32) {
-  //       api.applyImpulse([0, 5, 0], [0, 0, 0])
-  //     }
-  //   }
-  //   document.addEventListener('keydown', listener)
-  // }, [])
-  const toMoveRef = useRef<Group>();
 
-  const pos = useSpring(0, { damping: 1, stiffness: 1 });
-  //   const pos = useSpring(0);
-  var theta = 0;
-  var dTheta = (2 * Math.PI) / 1000;
+function rotateAroundWorldAxis( obj: Group, point: Vector3, axis: Vector3, angle: number ) {
+  var q = new Quaternion();
+  const test = new Vector3()
+    q.setFromAxisAngle( axis, angle );
+
+    obj.applyQuaternion( q );
+
+    obj.position.sub( point );
+    obj.position.applyQuaternion( q );
+    obj.position.add( point );
+    obj.updateWorldMatrix(true, true)
+    obj.updateMatrixWorld()
+    obj.localToWorld(test)
+    const ht = obj.children[0].children[0];
+
+    ht.updateMatrixWorld()
+    ht.localToWorld( test );
+    // console.log(test);
+    return test;
+}
+
+
+const World = ({ trig }: { trig: number }): JSX.Element => {
+  // const toMoveRef = useRef<Group>();
+  const htmlGroupRef = useRef<Group>();
+  const secondGroupRef = useRef<Group>();
+  const thirdGroupRef = useRef<Group>();
+
+  const pos = useSpring(0);
+  const yPos = useSpring(0)
+  const xPos = useSpring(0)
+  
+  // const pos = useSpring(0, { damping: 1000, stiffness: 100 });
+  // const pos = useSpring(0);
   useEffect(() => {
     console.log(trig);
 
-    const val = (pos.get() + (2 * Math.PI) / 3) % (2 * Math.PI);
-    console.log(val);
+    const val = pos.get()+((2*Math.PI)/3)
     pos.set(val);
-    toMoveRef.current.applyMatrix4(new Matrix4().makeRotationX(Math.PI / 8));
-    toMoveRef.current.applyMatrix4(new Matrix4().makeTranslation(0.5, 1, -0.5));
+    
+
   }, [trig]);
 
-  pos.onChange((last) => {
-    if (toMoveRef.current) {
-      const scale = 20.0;
-      //   //   toMoveRef.current.translateZ(Math.cos(last) * scale);
-      //   console.log(last);
-      //   //   toMoveRef.current.translateY(last);
-      //   const oPos = toMoveRef.current.position;
-      //   const x = toMoveRef.current.position.x;
-      //   //   //   const x = position
-      //   //   //   const y = toMoveRef.current.position.y;
-      //   const oldy = toMoveRef.current.position.y;
-      //   const oldz = toMoveRef.current.position.z;
-      //   const y = Math.cos(last) * scale;
-      //   //   const z = toMoveRef.current.position.z;
-      //   const z = Math.sin(last) * scale;
-      //   toMoveRef.current.position.set(0, 5, 0);
-      //   toMoveRef.current.position.set(x, y, z);
-      //   toMoveRef.current.position.set(x, y + oldy, z + oldz);
-      // theta += dTheta;
-      // // const x = 50 * Math.cos(theta);
-      // const y = 50 * Math.cos(theta);
-      // toMoveRef.current.position.set(x, y, z);
-      //   toMoveRef.current.translateY(Math.sin(pos.get()) * scale);
-      //   const z = Math.cos(last) * scale;
-      //   console.log(z);
-    }
-  });
+  // pos.onChange((last) => {
+  //   if (toMoveRef.current) {
+  //     const targ = toMoveRef.current.position
+  //     const p = new Vector3(0, 0, 0);
+  //     const ax = new Vector3(1, 0, 0);
+  //     const r = .5;
+  //     const a =   r * Math.PI * 2 / last
+  //     // console.log(last);
+  //     // rotateAroundWorldAxis(toMoveRef.current, p, ax, Math.sin(last))
+  //     const v = Math.sin(last%Math.PI);
+  //     // console.log(v);
+  //     console.log(last%Math.PI);
+      
+  //     const val = rotateAroundWorldAxis(toMoveRef.current, p, ax, last)
+  //     console.log(val)
+  //     // yPos.set(val.y)
+  //     // xPos.set(val.z)
+  //     // htmlGroupRef.current.position.set(0, 0, val.z)
 
-  useEffect(() => {
-    console.log();
+  //   }
+  // });
 
-    if (toMoveRef.current) {
-      // const time = state.clock.getElapsedTime();
-      // toMoveRef.current.translateZ(Math.cos(time) * scale);
-      //   toMoveRef.current.rotateOnAxis(0, -4.5, 0);
-      //   toMoveRef.current.position.set(0, -4.5, 0);
-      //   toMoveRef.current.position.set(0, 0, -4.5);
-      //   toMoveRef.current.position.set(-4.5, 0, 0);
-      //   toMoveRef.current.position.x += 0.1;
-      // toMoveRef.current.
-    }
-  }, []);
+  // yPos.onChange((last)=>{
+  //         htmlGroupRef.current.position.set(0, last, xPos.get())
+  // })
+//   xPos.onChange((last)=>{
+//     htmlGroupRef.current.position.set(last, last, 0)
+
 
   const boxRef = useRef<Mesh>();
   var rotation = new Matrix4().makeRotationZ(Math.PI / 2);
@@ -106,61 +112,52 @@ const World = ({ trig }: { trig: number }): JSX.Element => {
   var vector = new Vector3(0, 0, 1);
   let axis = new Vector3(0, 3, 0);
   useFrame((state, delta) => {
-    if (toMoveRef.current) {
+    // if (toMoveRef.current) {
+   
       const time = state.clock.getElapsedTime();
-      const scale = 0.2;
-      //   vector.applyAxisAngle(axis, Math.sin(time * 0.01));
-      //   vector.applyAxisAngle(axis, Math.sin(time * 0.01));
-      console.log(vector.x);
-      //   boxRef.current.position.set(vector.x, vector.y, vector.z);
-      //   let transformation = new Matrix4().makeTranslation(0, 0.1, 0);
-      //   boxRef.current.applyMatrix4(rotation.multiply(translation));
-      //   boxRef.current.applyMatrix4(rotation);
-      //   toMoveRef.current.translateOnAxis(
-      //     new Vector3(0, Math.cos(time) * scale, 0),
-      //     Math.cos(time) * scale
-      //   );
-      //   toMoveRef.current.rotateOnAxis(
-      //     new Vector3(0, -4.5, 0),
-      //     Math.cos(time) * scale
-      //   );
-      //   console.log(state);
-      //   console.log(state);
-      //   toMoveRef.current.translateX(Math.sin())
-      //   toMoveRef.current.translateX(1);
-      //   toMoveRef.current.translateX(Math.sin(time) * 0.5);
-      //   var desiredTransform = new Matrix4().makeRotationY(Math.sin(time));
-      //   desiredTransform = desiredTransform.makeTranslation(5, 5, 5);
-      const z = Math.cos(time) * scale;
-      //   console.log(z);
-      //   toMoveRef.current.translateZ(Math.cos(time) * scale);
-      toMoveRef.current.rotateX(Math.sin(time * 0.01) * scale);
-      //   toMoveRef.current.rotateX(desiredTransform);
-      //   toMoveRef.current.applyMatrix4(desiredTransform);
-      // toMoveRef.current.position.x += 0.1;
-      // toMoveRef.current.
-    }
+      const r = 10.5;
+      const a =   r * Math.PI * 2 / time
+      let val = 0;
+      val += pos.get()
+      const third = (2*Math.PI)/3;
+      const off = third/2
+      val+= off;
+      htmlGroupRef.current.position.set(0, Math.cos(val)*r, Math.sin(val)*r)
+    
+      secondGroupRef.current.position.set(0, Math.cos(val+third)*r, Math.sin(val+third)*r)
+      thirdGroupRef.current.position.set(0, Math.cos(val+third*2)*r, Math.sin(val+third*2)*r)
+    // }
   });
 
   return (
     <>
       <group>
-        <group position={[0, 20, 0]}>
-          <Html transform position={[0, 0, 0]} occlude center>
+        <gridHelper/>
+        {/* <group ref={htmlGroupRef}>
+          <Html transform  occlude center>
             <div style={{ width: 400, height: 200, backgroundColor: "red" }}>
               <p>{lorem}</p>
             </div>
           </Html>
         </group>
-        <group ref={toMoveRef}>
-          <group position={[0, 20, 0]}>
-            <mesh position={[0, 0, 0]} ref={boxRef}>
-              <boxBufferGeometry args={[10, 10, 10]} attach="geometry" />
-              <meshPhongMaterial color="#18a36e" attach="material" />
-            </mesh>
-          </group>
+        <group  ref={secondGroupRef}>
+          <Html transform occlude center>
+            <div style={{ width: 400, height: 200, backgroundColor: "green" }}>
+              <p>{lorem}</p>
+            </div>
+          </Html>
         </group>
-        <Html transform position={[0, 0, 0]}>
+        <group  ref={thirdGroupRef} >
+          <Html transform  occlude center>
+            <div style={{ width: 400, height: 200, backgroundColor: "green" }}>
+              <p>{lorem}</p>
+            </div>
+          </Html>
+        </group> */}
+        <DivBlock color="red" ref ={htmlGroupRef}/>
+        <DivBlock color="green" ref = {secondGroupRef}/>
+        <DivBlock color="blue" ref = {thirdGroupRef}/>
+        <Html transform >
           <div
             style={{ width: 1000, height: 200, backgroundColor: "blue" }}
           ></div>
@@ -169,6 +166,44 @@ const World = ({ trig }: { trig: number }): JSX.Element => {
     </>
   );
 };
+
+// const DivBlock = ({color}:{color: string}): JSX.Element=>{
+//   return(
+//     <Html transform position={[0, 0, 0]}>
+//     <div
+//       style={{ width: 1000, height: 200, backgroundColor: color }}
+//     ></div>
+//   </Html>
+//   )
+// }
+
+const DivBlock = forwardRef(function DivBlock({color}:{color: string}, forwardRef) {
+  const points = [
+    [0, -10, 10],
+    // [10, 0, 10],
+  ].map(([x, y, z]) => new Vector3(x, y, z));
+  
+  
+  const { point, point2 } = useControls({ point: [-2, -2, 1], point2: [10, 0, 10]  });
+  const curve3 = useMemo(
+    () => new CatmullRomCurve3([new Vector3(...point), points[0]]),
+    [point]
+  );
+  
+  return (
+    <group ref = {forwardRef}>
+     <Tube args={[curve3, undefined, 0.1]}>
+      <meshBasicMaterial color={"red"} />
+    </Tube>
+    <Html transform position={[0, 0, 0]}>
+ 
+    <div
+      style={{ width: 1000, height: 200, backgroundColor: color }}
+    >{lorem}</div>
+  </Html>
+  </group>
+  );
+});
 
 const CSS3DDemo = (): JSX.Element => {
   const [trig, setTrig] = useState(0);
